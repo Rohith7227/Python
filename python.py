2d2d156bb468f76c387038c24854d500bb391248
Rohith7227 <165925376+Rohith7227@users.noreply.github.com>
2026-09-15 16:42:21 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
+score = int(input("Enter your score": ))
+
+  if score >= 90:
+    print("Grade A+")
+  if score >= 80:
+    print("Grade A")
+  if score >= 70:
+    print("Grade B+")
+  if score >= 60:
+    print("Grade B")
+  else:
+    print("Grade F")
 
