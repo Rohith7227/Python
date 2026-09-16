5c5b7437b227c8e7ec9bc16eaaa4ed6346ba9c0e
Rohith7227 <165925376+Rohith7227@users.noreply.github.com>
2026-09-16 12:33:12 +0530

    Add ATM simulation with withdrawal logic
    Implement basic ATM simulation with card check, PIN validation, and withdrawal functionality.

--- ATM Simulation.py
+++ ATM Simulation.py
@@ -1,1 +1,20 @@
+#ATM Simulation
 
+has_card = True
+pin = 123
+correct_pin = 123
+balance = 57000
+withdraw_amount = 8000
+
+if has_card:
+  if pin == correct_pin:
+    if withdraw_amount <= balance:
+      print(f"balance = {balance}")
+      balance -= withdraw_amount
+      print(f"remaining amount : {balance}")
+      else:
+      print("insufficient amount")
+  else:
+    print("incorrect pin")
+else:
+  print("insert card")
